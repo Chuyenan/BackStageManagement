ef28cdb1859d82d528adf0188e49a72651eea4e0
zcy <1491135894@qq.com>
2020-06-24 19:17:17 +0800

    赵晨宇第一次上传（文件结构）1.0 2.0xiugai

--- GStore_Web/src/main/java/com/npu/TestServlet.java
+++ GStore_Web/src/main/java/com/npu/TestServlet.java
@@ -7,11 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("TestServlet")
+@WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
         req.getRequestDispatcher("success.jsp").forward(req, resp);
     }
 }
--- GStore_Web/src/main/webapp/index.jsp
+++ GStore_Web/src/main/webapp/index.jsp
@@ -11,6 +11,6 @@
     <title>Title</title>
 </head>
 <body>
-<a href="${pageContext.request.contextPath}/testS">press it</a>
+<a href="TestServlet">press it !!!!</a>
 </body>
 </html>
